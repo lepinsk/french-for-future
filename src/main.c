4e3e896428d969e9fb34312709cb8bfcebea1c9c
Julian Lepinski <lepinski@gmail.com>
2014-02-14 14:21:39 -0800

    holy fuck, done.

--- src/main.c
+++ src/main.c
@@ -8,6 +8,8 @@
 #define DAY_FRAME       (GRect(0, 17, 144, 168-62))
 #define TIME_FRAME      (GRect(0, 33, 144, 168-20))
 #define DATE_FRAME      (GRect(0, 90, 144, 168-62))
+#define TEMP_FRAME      (GRect(10, 138, 134, 168-62))
+#define COND_FRAME      (GRect(0, 138, 134, 168-62))
 
 /* Keep a pointer to the current weather data as a global variable */
 static WeatherData *weather_data;
@@ -17,11 +19,18 @@ static Window *window;
 static TextLayer *date_layer;
 static TextLayer *day_layer;
 static TextLayer *time_layer;
+static TextLayer *temp_layer;
+static TextLayer *cond_layer;
 static WeatherLayer *weather_layer;
+
+GBitmap *line_bmp;
+BitmapLayer *line_layer;
 
 static char date_text[] = "XXXXXXXXX 00";
 static char day_text[] = "XXXXXXXXX";
 static char time_text[] = "00:00";
+static char temp_text[] = "XXXX";
+//static char cond_text[] = "XXXXXXXXX";
 
 /* Preload the fonts */
 GFont font_date;
@@ -67,6 +76,9 @@ static void handle_tick(struct tm *tick_time, TimeUnits units_changed)
     strftime(day_text_pre, sizeof(day_text_pre), "%A", tick_time);
     snprintf(day_text, sizeof(day_text), "%s", day_text_pre);
     text_layer_set_text(day_layer, upcase(day_text));
+
+    text_layer_set_text(temp_layer, "-99°");
+    text_layer_set_text(cond_layer, "LOADING  ");
   }
 
   // Update the bottom half of the screen: icon and temperature
@@ -75,20 +87,20 @@ static void handle_tick(struct tm *tick_time, TimeUnits units_changed)
   {
     // 'Animate' loading icon until the first successful weather request
     if (animation_step == 0) {
-      weather_layer_set_icon(weather_layer, WEATHER_ICON_LOADING1);
+      text_layer_set_text(cond_layer, "LOADING  ");
     }
     else if (animation_step == 1) {
-      weather_layer_set_icon(weather_layer, WEATHER_ICON_LOADING2);
+      text_layer_set_text(cond_layer, "LOADING. ");
     }
     else if (animation_step >= 2) {
-      weather_layer_set_icon(weather_layer, WEATHER_ICON_LOADING3);
+      text_layer_set_text(cond_layer, "LOADING..");
     }
     animation_step = (animation_step + 1) % 3;
   }
   else {
     // Update the weather icon and temperature
     if (weather_data->error) {
-      weather_layer_set_icon(weather_layer, WEATHER_ICON_PHONE_ERROR);
+      text_layer_set_text(cond_layer, "ERROR");
     }
     else {
       // Show the temperature as 'stale' if it has not been updated in 30 minutes
@@ -96,16 +108,70 @@ static void handle_tick(struct tm *tick_time, TimeUnits units_changed)
       if (weather_data->updated > time(NULL) + 1800) {
         stale = true;
       }
-      weather_layer_set_temperature(weather_layer, weather_data->temperature, stale);
+      //weather_layer_set_temperature(weather_layer, weather_data->temperature, stale);
+      
 
-      // Figure out if it's day or night. Not the best way to do this but the webservice
-      // does not seem to return this info.
-      /*
-      bool night_time = false;
-      if (tick_time->tm_hour >= 19 || tick_time->tm_hour < 7)
-        night_time = true;
-      */
-      weather_layer_set_condition(weather_layer, weather_data->condition);
+
+
+      int t = weather_data->temperature;
+      snprintf(temp_text, sizeof(temp_text), "%i%s", t, stale ? " " : "°");
+      text_layer_set_text(temp_layer, temp_text);
+      
+
+      int c = weather_data->condition;
+      if (c < 300) {
+        text_layer_set_text(cond_layer, "STORMY");
+      }
+      // Drizzle
+      else if (c < 500) {
+        text_layer_set_text(cond_layer, "DRIZZLE");
+      }
+      // Rain / Freezing rain / Shower rain
+      else if (c < 600) {
+        text_layer_set_text(cond_layer, "RAINY");
+      }
+      // Snow
+      else if (c < 700) {
+        text_layer_set_text(cond_layer, "SNOWY");
+      }
+      // Fog / Mist / Haze / etc.
+      else if (c < 771) {
+        text_layer_set_text(cond_layer, "FOGGY");
+      }
+      // Tornado / Squalls
+      else if (c < 800) {
+        text_layer_set_text(cond_layer, "WINDY");
+      }
+      // Sky is clear
+      else if (c == 800) {
+        text_layer_set_text(cond_layer, "CLEAR");
+      }
+      // few/scattered/broken clouds
+      else if (c < 804) {
+        text_layer_set_text(cond_layer, "P.CLOUDY");
+      }
+      // overcast clouds
+      else if (c == 804) {
+        text_layer_set_text(cond_layer, "CLOUDY");
+      }
+      // Extreme
+      else if ((c >= 900 && c < 903) || (c > 904 && c < 1000)) {
+        text_layer_set_text(cond_layer, "WINDY");
+      }
+      // Cold
+      else if (c == 903) {
+        text_layer_set_text(cond_layer, "COLD");
+      }
+      // Hot
+      else if (c == 904) {
+        text_layer_set_text(cond_layer, "HOT");
+      }
+      else {
+        text_layer_set_text(cond_layer, "HMM");
+      }
+
+
+      //weather_layer_set_condition(weather_layer, weather_data->condition);
     }
   }
 
@@ -148,9 +214,29 @@ static void init(void) {
   text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(date_layer));
 
+  temp_layer = text_layer_create(TEMP_FRAME);
+  text_layer_set_text_color(temp_layer, GColorWhite);
+  text_layer_set_background_color(temp_layer, GColorClear);
+  text_layer_set_font(temp_layer, font_date);
+  text_layer_set_text_alignment(temp_layer, GTextAlignmentLeft);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(temp_layer));
+
+  cond_layer = text_layer_create(COND_FRAME);
+  text_layer_set_text_color(cond_layer, GColorWhite);
+  text_layer_set_background_color(cond_layer, GColorClear);
+  text_layer_set_font(cond_layer, font_date);
+  text_layer_set_text_alignment(cond_layer, GTextAlignmentRight);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(cond_layer));
+
+  GBitmap *new_icon =  gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DOTS);
+  line_layer = bitmap_layer_create(GRect(8, 126, 128, 1));
+  bitmap_layer_set_bitmap(line_layer, new_icon);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(line_layer));
+
+
   // Add weather layer
   weather_layer = weather_layer_create(GRect(0, 90, 144, 80));
-  layer_add_child(window_get_root_layer(window), weather_layer);
+  //layer_add_child(window_get_root_layer(window), weather_layer);
 
   // Update the screen right away
   time_t now = time(NULL);
@@ -166,7 +252,10 @@ static void deinit(void) {
   text_layer_destroy(time_layer);
   text_layer_destroy(day_layer);
   text_layer_destroy(date_layer);
+  text_layer_destroy(temp_layer);
+  text_layer_destroy(cond_layer);
   weather_layer_destroy(weather_layer);
+  bitmap_layer_destroy(line_layer);
 
   fonts_unload_custom_font(font_date);
   fonts_unload_custom_font(font_time);
