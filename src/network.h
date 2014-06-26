77488c32b409e6eb91b6d7e94b4493091b2de2bb
Julian Lepinski <lepinski@gmail.com>
2014-06-26 17:43:21 -0400

    manually merging in all the changes from @katherine's futura-weather pull request
    fml.

--- src/network.h
+++ src/network.h
@@ -11,8 +11,11 @@
 // these are our dictionary keys – they all correspond to stuff in appinfo.json
 #define KEY_TEMPERATURE 0
 #define KEY_CONDITION 1
-#define KEY_ERROR 2
-#define KEY_COLOURSCHEME 3
+#define KEY_SUNRISE 2
+#define KEY_SUNSET 3
+#define KEY_CURRENT_TIME 4
+#define KEY_ERROR 5
+#define KEY_COLOURSCHEME 6
 #define KEY_REQUEST_UPDATE 42
 
 typedef enum {
@@ -25,11 +28,19 @@ typedef enum {
 typedef struct {
   int temperature;
   int condition;
+  int sunrise;
+  int sunset;
+  int current_time;
   time_t updated;
-  WeatherError error;
 } WeatherData;
 
-void (*cbf)(bool);
-void init_network(WeatherData *weather_data, void (*callbackFunction)(bool));
-void close_network();
-void request_weather();
+typedef void (*WeatherUpdateHandler)(WeatherData*);
+typedef void (*WeatherErrorHandler)(WeatherError);
+
+void init_network(void);
+void close_network(void);
+
+void request_weather(void);
+
+void set_weather_update_handler(WeatherUpdateHandler handler);
+void set_weather_error_handler(WeatherErrorHandler handler);
